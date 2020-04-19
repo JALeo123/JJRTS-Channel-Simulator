2b128b19fb74f073e233ca1304bce150c5e1e43a
Ryan <rtoffel@uccs.edu>
2020-04-19 16:40:50 -0600

    Faster time delay algorithm

--- ADC_DAC_DSP.py
+++ ADC_DAC_DSP.py
@@ -7,6 +7,9 @@ import asyncio
 import math
 import matplotlib.pyplot as plt
 from SoapySDR import SOAPY_SDR_RX, SOAPY_SDR_CS16, SOAPY_SDR_TX
+from itertools import cycle
+global keep_streaming
+
 #enumerate devices
 results = SoapySDR.Device.enumerate()
 for result in results: print(result)
@@ -54,6 +57,7 @@ print("\nBuffer Length:", len(buff1), "\n")
 
 #setup a stream (complex floats)
 rx_stream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CF32)
+
 tx_stream = sdr.setupStream(SOAPY_SDR_TX, SOAPY_SDR_CF32)
 time.sleep(1)
 
@@ -66,40 +70,47 @@ sdr.activateStream(rx_stream)#, numElems = len(buff1))
 print("Activation Complete")
 time.sleep(1)
 
-#Create class to initialize DSP object and such
-class delay(object): 
-	#initialize object
-	def __init__(self, delay_samps = 0, buff_size = buff_len, phase_index = 0):
-		# Time delay using circular buffer
-		self.delay = delay_samps
-		self.ptr = 0
-		self.buffsize = buff_size
-		self.cirbuff = numpy.array([0]*buff_size, numpy.complex64)
+#Create class to initialize DSP class
+class time_delay(object):
+   
+    def __init__ (self, delay_samps = 0, Nbuffer_len = 2048):
+        """
+        Initialize the object
+        """
+        self.delay = delay_samps
+        self.Nbuf = Nbuffer_len
+        self.ping = numpy.zeros(Nbuffer_len, numpy.complex64)
+        self.pong = numpy.zeros(Nbuffer_len, numpy.complex64)
+        
+    def set_delay(self,new_delay):
+        self.delay = new_delay
+    
+    def process_frame(self,x):
+        """
+        Process a frame of samples
+        """
+        global pingpong, myIterator
+        NFrame = len(x)
+        y = numpy.zeros(NFrame, numpy.complex64)
+        p = numpy.zeros(NFrame, numpy.complex64)
+        pingpong = next(myIterator)
+        
+        if pingpong == 0:
+            self.ping = x
+            p = numpy.resize(numpy.roll(self.pong, self.delay), (1, self.delay))
+            y = numpy.append(p, numpy.resize(x, (1, len(x) - self.delay)))   
+        else: 
+            self.pong = x
+            p = numpy.resize(numpy.roll(self.ping, self.delay), (1, self.delay))
+            y = numpy.append(p, numpy.resize(x, (1, len(x) - self.delay)))
+            
+        return y
 
-	#Set user defines delay
-	def set_delay(self, new_delay):
-		self.delay = new_delay
-
-	def process_sample(self, buff):
-		self.cirbuff[self.ptr] = buff
-		buffd = self.cirbuff[(self.ptr - self.delay) % self.buffsize]
-		self.ptr = (self.ptr + 1) % self.buffsize
-
-		return buffd
-
-	def process_frame(self, buff):
-		Nframe = len(buff)
-		buffd = numpy.array([0]*Nframe, numpy.complex64)
-		for k in range(Nframe):
-			buffd[k] = self.process_sample(buff[k])
-
-		return buffd
-
-#Create class to initialize DSP object and such
+#Create class to initialize DSP class
 class phase_shift(object): 
 
 	#initialize object
-	def __init__(self, buff_size = buff_len, phase_index = 0):
+	def __init__(self, buff_size = 4096, phase_index = 0):
 
 		#Phase shift using complex multiplication/LUTs
 		self.ptr = 0
@@ -125,41 +136,25 @@ pi = math.pi
 pingpong = 0
 rqs_delay = 0
 rqs_phase = 0
-cbuf1 = delay(0, buff_len)
+td1 = time_delay(0, buff_len)
 phase1 = phase_shift(buff_len, 0)
 keep_streaming = True
+
+#Globals used for time delay
+pingpong = 0
+myIterator = cycle(range(2))
+
 #infinite loop - break by changing keep_streaming = False
 while(keep_streaming):
-
-	#rqs_delay and rqs_phase variables are set by ethernet command
-	cbuf1.set_delay(rqs_delay)
+	#Set delay and phase values
+	td1.set_delay(rqs_delay)
 	phase1.set_phase(rqs_phase)
-	
-	if pingpong == 0:
-		sr_read = sdr.readStream(rx_stream, [buff1], len(buff1))
-		sr_write = sdr.writeStream(tx_stream, [buff2], len(buff2))
 
-		#Process signal - processing delay = 9ms worst case
-		#Processing time measured using time.time() and the result is printed, which may skew the results
-		# start_time = time.time()
-		buff1 = cbuf1.process_frame(buff1)
-		buff1 = phase1.process_frame(buff1)
-		#end_time = time.time()
-		#print("Processing Time = ", end_time - start_time)
-		pingpong = 1
-		
-	else: 
-		sr_read = sdr.readStream(rx_stream, [buff2], len(buff2))
-		sr_write = sdr.writeStream(tx_stream, [buff1], len(buff1))
-
-		#Process signal - processing delay = 9ms worst case
-		#Processing time measured using time.time() and the result is printed, which may skew the results
-		# start_time = time.time()
-		buff2 = cbuf1.process_frame(buff2)                                                                                         
-		buff2 = phase1.process_frame(buff2)
-		#end_time = time.time()
-		#print("Processing Time = ", end_time - start_time)
-		pingpong = 0
+	#stream/DSP
+	sr_read = sdr.readStream(rx_stream, [buff1], len(buff1))
+	buff1 = td1.process_frame(buff1)
+	buff1 = phase1.process_frame(buff1)
+	sr_write = sdr.writeStream(tx_stream, [buff1], len(buff1))
 
 print("Closing Streams")
 sdr.deactivateStream(rx_stream)
